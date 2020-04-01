1fd9c9467d2d921a5e9ba58c06850363f9672f78
Joseph Avetsev <avetsev@gmail.com>
2020-04-01 13:28:41 +0300

    .gitignore

---
+++ nfa.py
@@ -0,0 +1,7 @@
+class Nfa:
+    def __init__(self, states, alphabet, transitions, initial_state, accepting_states):
+        self.states = states
+        self.alphabet = alphabet
+        self.transitions = transitions
+        self.initial_state = initial_state
+        self.accepting_states = accepting_states
