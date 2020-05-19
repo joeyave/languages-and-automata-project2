88f3fa321cb936b9aa5d4dc9ee59100acc313216
Joseph Aveltsev <aveltsev@gmail.com>
2020-05-19 12:38:16 +0300

    epsilon closure endless recursion fix

--- nfa.py
+++ nfa.py
@@ -2,14 +2,17 @@ import json
 from itertools import chain
 
 
-def epsilon_closure(state, transitions):
+def epsilon_closure(state, transitions, visited=[]):
     closure = []
-    for substate in state:
-        if (substate, "eps") in transitions:
-            closure.append(substate)
-            closure.extend(epsilon_closure(transitions[(substate, "eps")], transitions))
-        if substate not in closure:
-            closure.append(substate)
+    if state not in visited:
+        visited.append(state)
+        for substate in state:
+            if (substate, "eps") in transitions:
+                closure.append(substate)
+                closure.extend(epsilon_closure(transitions[(substate, "eps")], transitions))
+            if substate not in closure:
+                closure.append(substate)
+    visited[:] = []
     return closure
 
 
@@ -27,12 +30,10 @@ class Nfa:
 
     def convert_to_dfa(self, file_name):
         dfa_alphabet = self.alphabet
-        dfa_t_func = []
+        dfa_transitions = {}
         dfa_initial_state = epsilon_closure([self.initial_state], self.transitions)
         dfa_accepting_states = []
         dfa_states = [tuple(dfa_initial_state)]
-
-        dfa_transitions = {}
 
         for dfa_state in dfa_states:
             for dfa_symbol in dfa_alphabet:
@@ -44,29 +45,31 @@ class Nfa:
                             epsilon_transition_state = epsilon_closure([transition], self.transitions)
                             if epsilon_transition_state not in new_state:
                                 new_state.append(epsilon_transition_state)
-                            # if self.transitions[(transition, "eps")] not in new_state:
-                            #     new_state.append(self.transitions[(transition, "eps")])
 
                 if new_state:
                     new_state = list(chain.from_iterable(new_state))
                     dfa_transitions[(dfa_state, dfa_symbol)] = new_state
 
-                    if tuple(new_state) not in dfa_states:
-                        dfa_states.append(tuple(new_state))
+                else:
+                    new_state.append("null")
+                    dfa_transitions[(dfa_state, dfa_symbol)] = ["null"]
+
+                if tuple(new_state) not in dfa_states:
+                    dfa_states.append(tuple(new_state))
 
         # Writing to JSON file.
+        temp_list = []
         for key, value in dfa_transitions.items():
-            temp_list = [["-".join(key[0]), key[1], "-".join(value)]]
-            dfa_t_func.extend(temp_list)
+            temp_list.extend([["-".join(key[0]), key[1], "-".join(value)]])
 
         for dfa_state in dfa_states:
             for accepting_state in self.accepting_states:
-                if accepting_state in dfa_state:
+                if accepting_state in dfa_state and "-".join(dfa_state) not in dfa_accepting_states:
                     dfa_accepting_states.append("-".join(dfa_state))
 
         dfa = dict()
         dfa["alphabet"] = dfa_alphabet
-        dfa["transitions"] = dfa_t_func
+        dfa["transitions"] = temp_list
         dfa["initial_state"] = "-".join(dfa_initial_state)
         dfa["accepting_states"] = dfa_accepting_states
 
